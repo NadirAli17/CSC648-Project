d49837f49f22030428b4ed9622f8d38f97b0f8b3
Aidan <abayercalvert@gmail.com>
2024-09-12 23:19:31 -0700

    Fix to Aidan about us role

--- web/src/AboutUs.js
+++ web/src/AboutUs.js
@@ -1,23 +1,36 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from 'react'
+import { Link } from 'react-router-dom'
 
 const AboutUs = () => {
   const teamMembers = [
-    { name: "Andrew Dahlstrom", role: "Product Owner", link: "/team/andrew-dahlstrom" },
-    { name: "Jason Avina", role: "Scrum Master", link: "/team/jason-avina" },
-    { name: "Joyce Fu", role: "Scrum Master", link: "/team/joyce-fu" },
-    { name: "Nadir Ali", role: "Github Master", link: "/team/nadir-ali" },
-    { name: "Kayla Maa", role: "Front-end Lead", link: "/team/kayla-maa" },
-    { name: "Aidan Bayer-Calvert", role: "Front-end Lead", link: "/team/aidan-bayer-calvert" },
-    { name: "Ryan Flannery", role: "Back-end Lead", link: "/team/ryan-flannery" },
-  ];
+    {
+      name: 'Andrew Dahlstrom',
+      role: 'Product Owner',
+      link: '/team/andrew-dahlstrom',
+    },
+    { name: 'Jason Avina', role: 'Scrum Master', link: '/team/jason-avina' },
+    { name: 'Joyce Fu', role: 'Scrum Master', link: '/team/joyce-fu' },
+    { name: 'Nadir Ali', role: 'Github Master', link: '/team/nadir-ali' },
+    { name: 'Kayla Maa', role: 'Front-end Lead', link: '/team/kayla-maa' },
+    {
+      name: 'Aidan Bayer-Calvert',
+      role: 'Back-end Lead',
+      link: '/team/aidan-bayer-calvert',
+    },
+    {
+      name: 'Ryan Flannery',
+      role: 'Back-end Lead',
+      link: '/team/ryan-flannery',
+    },
+  ]
 
   return (
     <div className="about-us">
       <h1>About Us</h1>
       <p>
-        Welcome to our project! We are a group of 7 dedicated team members working to create
-        an amazing web application that helps users manage and track their career development.
+        Welcome to our project! We are a group of 7 dedicated team members
+        working to create an amazing web application that helps users manage and
+        track their career development.
       </p>
       <h2>Meet Our Team</h2>
       <ul>
@@ -25,12 +38,13 @@ const AboutUs = () => {
           <li key={index}>
             <Link to={member.link}>
               <strong>{member.name}</strong>
-            </Link> - {member.role}
+            </Link>{' '}
+            - {member.role}
           </li>
         ))}
       </ul>
     </div>
-  );
-};
+  )
+}
 
-export default AboutUs;
+export default AboutUs
--- web/src/individual-pages/AidanBayerCalvert.jsx
+++ web/src/individual-pages/AidanBayerCalvert.jsx
@@ -1,11 +1,14 @@
-import React from 'react';
+import React from "react";
 
 const AidanBayerCalvert = () => {
   return (
     <div className="team-member">
       <h1>Aidan Bayer-Calvert</h1>
       <h2>Front-end Lead</h2>
-      <p>Aidan is a Front-end Lead, working closely with Kayla to design and implement intuitive user interfaces.</p>
+      <p>
+        Aidan is a back-end lead, working closely with Ryan to ensure seamless
+        server-side functionality and data management.
+      </p>
     </div>
   );
 };
