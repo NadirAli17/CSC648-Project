dd1710d6debf90715e136b13b9be2cabc07a1826
JasonAvina <jason.avina@gmail.com>
2024-09-15 17:08:48 -0700

    my first commit to my branch on my about me page"

--- web/src/individual-pages/JasonAvina.jsx
+++ web/src/individual-pages/JasonAvina.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import jasonImageImage from '../assets/images/jason-avina.png'; // Import the image
 
 const JasonAvina = () => {
   return (
@@ -6,11 +7,14 @@ const JasonAvina = () => {
       <h1>Jason Avina</h1>
       <h2>Scrum Master</h2>
       <p>Jason is one of the Scrum Masters, ensuring the smooth operation of the team’s processes and workflows.</p>
-      <p>BIO HERE</p>
-      {/* <img
-        src="path to image"
-        alt="img"
-      /> */}
+      <p>I am a graduate student in the Data Science and Artificial Intelligence program here at SFSU. I like working out,
+        trading stocks, and am learning Mandarin. 
+      </p>
+      <img
+        src={jasonImage}  // Use the imported image
+        alt="Jason Avina"
+        style={{ width: '200px', height: 'auto' }} // You can adjust size as needed
+      />
     </div>
   );
 };
