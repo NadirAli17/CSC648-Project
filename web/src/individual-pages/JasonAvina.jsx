0528ced7167d135ab94a306ed892aa094f412b04
JasonAvina <jason.avina@gmail.com>
2024-09-16 12:31:41 -0700

    updated my page on the main branch

--- web/src/individual-pages/JasonAvina.jsx
+++ web/src/individual-pages/JasonAvina.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import jasonImage from '../assets/images/jason-avina.png'; // Import the image
+
 
 const JasonAvina = () => {
   return (
@@ -6,11 +8,12 @@ const JasonAvina = () => {
       <h1>Jason Avina</h1>
       <h2>Scrum Master</h2>
       <p>Jason is one of the Scrum Masters, ensuring the smooth operation of the team’s processes and workflows.</p>
-      <p>BIO HERE</p>
-      {/* <img
-        src="path to image"
-        alt="img"
-      /> */}
+      <p>Hello, I am a graduate student in the Data Science and Artificial Intelligence program at SFSU. My other interests include stock trading, exploring SF and learning Mandarin.</p>
+      <img
+        src={jasonImage}  // Use the imported image
+        alt="Andrew Dahlstrom"
+        style={{ width: '200px', height: 'auto' }} // You can adjust size as needed
+      />
     </div>
   );
 };
