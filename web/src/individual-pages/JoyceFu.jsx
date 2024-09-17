6bbdf71dd8b072edb10f0a4ecebf9d87acaa2dfc
fufu911219 <143296471+fufu911219@users.noreply.github.com>
2024-09-16 17:11:32 -0700

    Update JoyceFu.jsx

--- web/src/individual-pages/JoyceFu.jsx
+++ web/src/individual-pages/JoyceFu.jsx
@@ -6,7 +6,7 @@ const JoyceFu = () => {
       <h1>Joyce Fu</h1>
       <h2>Scrum Master</h2>
       <p>Joyce is a dedicated Scrum Master, facilitating meetings and improving team productivity.</p>
-      <p>BIO HERE</p>
+      <p>I'm Joyce from Taiwan. I'm currently a senior majoring in Computer Science at SFSU. In my leisure time, I enjoy going out with friends and indulging in delicious food.</p>
       {/* <img
         src="path to image"
         alt="img"
