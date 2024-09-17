6eb94db2241a45ae661025ecdf29f16d9bf86f23
Nadir Ali <nali1@sfsu.edu>
2024-09-16 23:46:15 -0700

    Nadir bio

--- web/src/individual-pages/NadirAli.jsx
+++ web/src/individual-pages/NadirAli.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import nadirAliImg from '../assets/images/NadirAli.JPG';
 
 const NadirAli = () => {
   return (
@@ -6,11 +7,14 @@ const NadirAli = () => {
       <h1>Nadir Ali</h1>
       <h2>Github Master</h2>
       <p>Nadir is responsible for managing the GitHub repository and ensuring version control best practices are followed.</p>
-      <p>BIO HERE</p>
-      {/* <img
-        src="path to image"
-        alt="img"
-      /> */}
+      <p>Hello everyone! My name is Nadir and I ama senior at San Franciso State University.
+        In my freetime I like to play soccer and watch tv shows. Some of my favorite shows inlcude Breaking Bad and Game of Thrones
+      </p>
+      <img
+        src={nadirAliImg}  // Use the imported image
+        alt="Nadir Ali"
+        style={{ width: '200px', height: 'auto' }} // You can adjust size as needed
+      />
     </div>
   );
 };
