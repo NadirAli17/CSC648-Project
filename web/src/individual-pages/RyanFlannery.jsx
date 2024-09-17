c9782d9634756dade8f340719b1ca3f596caf060
ryanvflannery <ryanvflannery@gmail.com>
2024-09-17 02:06:02 -0700

    Ryan's Bio

--- web/src/individual-pages/RyanFlannery.jsx
+++ web/src/individual-pages/RyanFlannery.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import RyanImage from '../assets/images/RyanFlanneryI.jpg' // Import my image
 
 const RyanFlannery = () => {
   return (
@@ -9,11 +10,16 @@ const RyanFlannery = () => {
         Ryan leads the back-end development, ensuring server-side functionality
         and data management are seamless.
       </p>
-      <p>BIO HERE</p>
-      {/* <img
-        src="path to image"
-        alt="img"
-      /> */}
+      <p>
+        Hey everyone, my name is Ryan and I am a senior at SFSU majoring in Computer Science
+        with a minor in Computing Applications. My favorite things to do include going to the gym 
+        and playing sports, spending time with family, and developing video games in my free time.
+      </p>
+      <img
+        src={RyanImage} // Use imported image
+        alt="Ryan Flannery"
+        style={{ width: '200px', height: 'auto' }} 
+      /> 
     </div>
   );
 };
