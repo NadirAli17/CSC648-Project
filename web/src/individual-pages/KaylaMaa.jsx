12aa97259ff52d4e33ba173a327041a097e9813a
Kayla Maa <kaylacm11@gmail.com>
2024-09-16 17:59:15 -0700

    bio

--- web/src/individual-pages/KaylaMaa.jsx
+++ web/src/individual-pages/KaylaMaa.jsx
@@ -6,7 +6,7 @@ const KaylaMaa = () => {
       <h1>Kayla Maa</h1>
       <h2>Front-end Lead</h2>
       <p>Kayla leads the front-end development, focusing on delivering a user-friendly interface for the application.</p>
-      <p>Hello, I’m currently a senior at SF State, majoring in Computer Science. Outside of school, I enjoy exploring new places and being outdoors alongside my friends. </p>
+      <p>Hello, I’m currently a senior at SF State, majoring in Computer Science. Outside of school, I enjoy exploring new places and being outdoors alongside my friends! </p>
       {/* <img
         src="path to image"
         alt="img"
