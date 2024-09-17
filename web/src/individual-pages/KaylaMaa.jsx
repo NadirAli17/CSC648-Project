1dc7890322ada9ebdfb38e9c6f3759ec5ac363b3
Kayla Maa <kaylacm11@gmail.com>
2024-09-16 17:27:05 -0700

    update bio

--- web/src/individual-pages/KaylaMaa.jsx
+++ web/src/individual-pages/KaylaMaa.jsx
@@ -6,7 +6,7 @@ const KaylaMaa = () => {
       <h1>Kayla Maa</h1>
       <h2>Front-end Lead</h2>
       <p>Kayla leads the front-end development, focusing on delivering a user-friendly interface for the application.</p>
-      <p>BIO HERE</p>
+      <p></p>
       {/* <img
         src="path to image"
         alt="img"
